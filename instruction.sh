992bc637ea598075664d72dc521770bc339364bb
Ubuntu <ubuntu@ip-172-31-10-61.ec2.internal>
2023-03-24 01:03:09 +0000

    Created a bash script

---
+++ instruction.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#OWNER: OLUWANIFEMI OLUWAGBEMILA
+#DATE: MARCH 23 2023
+#DESCRIPTION: Introduction into automation using bash shell scripting. This file will
+#contain various linux command to test run bash scripting.
+
+
+
+
+
+#greetings from the machine
+echo "Hi there! I hope you are doing well today"
+sleep 5
+clear
+
+
+
+
+
+#This command is meant to reveal the current user signed into this machine.
+echo "You are the user called" && whoami
+#whoami
+sleep 5
+clear
+
+
+
+
+#This command reveals the present working directory.
+echo "You are currently working in the directory shown below"
+pwd
+sleep 5
+clear
+
+
+
+
